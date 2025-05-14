9ecdcb9ea7eb5ea4af29a62d76685286c7f5c756
SENA <SENA@IBAAPRCIIFSP409.FormacionSena.RED>
2025-05-14 17:51:43 -0500

    actualizacion

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    require_once 'Controlador/Controlador.php';
+    require_once 'Modelo/GestorCita.php';
+    require_once 'Modelo/Cita.php';
+    require_once 'Modelo/Paciente.php';
+    require_once 'Modelo/Conexion.php';
+
+    $controlador = new Controlador();
+
+    if( isset($_GET["accion"])){
+        if($_GET["accion"] == "asignar"){
+            $controlador->verPagina('Vista/html/asignar.php');
+        }
+        elseif($_GET["accion"] == "consultar"){
+            $controlador->verPagina('Vista/html/consultar.php');
+        }
+        elseif($_GET["accion"] == "cancelar"){
+            $controlador->verPagina('Vista/html/cancelar.php');
+        }
+        elseif($_GET["accion"] == "guardarCita"){
+            $controlador->agregarCita(
+            $_POST["asignarDocumento"],
+            $_POST["medico"],
+            $_POST["fecha"],
+            $_POST["hora"],
+            $_POST["consultorio"]);
+        }
+        elseif($_GET["accion"] == "consultarCita"){
+            $controlador->consultarCitas($_POST["consultarDocumento"]);
+        }
+        elseif($_GET["accion"] == "cancelarCita"){
+            $controlador->cancelarCitas($_POST["cancelarDocumento"]);
+        }
+        elseif($_GET["accion"] == "consultarPaciente"){
+            $controlador->consultarPaciente($_GET["documento"]);
+        }
+        } 
+        else {
+            $controlador->verPagina('Vista/html/inicio.php');
+    }
+
+?>
---
+++ vista/js/script.js
@@ -0,0 +1,5 @@
+function consultarPaciente(){
+ var url = "index.php?accion=consultarPaciente&documento=" + $("#asignarDocumento").val();
+ $("#paciente").load(url,function(){
+ });
+}
